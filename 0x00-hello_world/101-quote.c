1e5c352a01f3bf800153b70af734b46b0ccb0bd0
Hope Yusuf <hopeyusufrya@gmail.com>
2023-02-19 03:33:45 -0800

    0x00. C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 /**
  * main - A C program that prints a line to the standard error
- * Return 1 (success)
- * */
+* Return: Always 1
+*/
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
