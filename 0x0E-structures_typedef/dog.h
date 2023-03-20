c7caf439b1c55d73a90dad009a9a0beff6318b4c
Hope Yusuf <hopeyusufrya@gmail.com>
2023-03-20 14:11:03 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H_
-#define DOG_H_
+#ifndef DOG_H
+#define DOG_H
 
 
 /**
@@ -7,6 +7,8 @@
  * @name: First member
  * @age: Second member
  * @owner: Third member
+ *
+ * Description: Longer description
  */
 struct dog
 {
@@ -15,14 +17,5 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - typedef for struct dog
- */
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
